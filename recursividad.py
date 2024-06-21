311c50edab79943315b7369f2a422e4a4f889d3b
Juan <jfhermoza@pucesi.edu.ec>
2024-06-20 19:54:48 -0500

    trabajo en clase

--- recursividad.py
+++ recursividad.py
@@ -1,2 +1,13 @@
-print("hola")
-print("cambios")
+def mostrar_numero(numero):
+    #caso base 
+    if numero <= 0:
+        return
+    #caso recursivo
+    else:
+        print( numero, end=" ,")
+        return mostrar_numero(numero-1)
+
+mostrar_numero(6)
+    
+
+#https://pythontutor.com/visualize.html#mode=edit
