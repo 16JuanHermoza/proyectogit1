99c976b70656e6b1fcd8dec7467c091d6bc52cc3
Juan <jfhermoza@pucesi.edu.ec>
2024-07-04 19:00:21 -0500

    base de cadenas

---
+++ cadenas.py
@@ -0,0 +1,16 @@
+def palindroma(lista):
+    while(len(lista)> 0):
+        if lista[0] == lista [-1]:
+            lista = lista[1:-1]
+            print(lista)
+        else:
+            print("no es palindroma")
+    if(len(lista)==0):
+        print("es palindroma")
+
+mi_lista = [1,2,3,3,2,1]
+print(palindroma(mi_lista))
+mi_lista2 = [1,6,3,6,2]
+print(palindroma(mi_lista2))
+ 
+
---
+++ lista.py
@@ -0,0 +1,3 @@
+mi_lista = [1,2,3,3,2,1]
+nueva_lista = mi_lista [1:-1]
+print(nueva_lista)
