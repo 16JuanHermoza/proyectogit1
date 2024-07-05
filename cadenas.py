c22e61d70f409858a76e7a39f399f4156dcc83e0
Juan <jfhermoza@pucesi.edu.ec>
2024-07-04 19:11:30 -0500

    ahora si final

--- cadenas.py
+++ cadenas.py
@@ -5,11 +5,24 @@ def palindroma(lista):
             print(lista)
         else:
             print("no es palindroma")
+            break
     if(len(lista)==0):
         print("es palindroma")
-
 mi_lista = [1,2,3,3,2,1]
 print(palindroma(mi_lista))
 
  
+def palindorma_recursiva(lista):
+    #caso base  
+    if len(lista) <=1:
+        return "es palindrome"
+    #recursividad
+    elif lista[0] == lista [-1]:
+        return palindorma_recursiva(lista[1:-1])
+    else:
+        return "no es palindrome"
+    
+mi_lista = [1,2,3,3,2,1]
+print(palindorma_recursiva(mi_lista))
+
 
