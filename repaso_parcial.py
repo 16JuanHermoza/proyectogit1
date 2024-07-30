7d1500f6dab112d4c99c3482fa90ef80480c998f
Juan <jfhermoza@pucesi.edu.ec>
2024-07-29 19:30:56 -0500

    cambio de int a object

--- repaso_parcial.py
+++ repaso_parcial.py
@@ -13,7 +13,7 @@ for i in range (matriz_b.shape[0]):
     for j in range(matriz_b.shape[1]):
         matriz_b[i,j] = random.randint(-50,50)
 
-matriz_r = np.zeros((filasa, columnasb), dtype=int)
+matriz_r = np.zeros((filasa, columnasb), dtype=object)
 
 print(f" A = \n{matriz_a}")
 print(f" B = \n{matriz_b}")
